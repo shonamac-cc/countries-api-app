b8bfe3581c57373f5bf802ad0575e6224780c933
Shona McKenzie <shona_mckenzie@hotmail.com>
2019-09-10 15:34:28 +0100

    initial setup

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import Vue from 'vue';
+
+document.addEventListener('DOMContentLoaded', () => {
+    new Vue({
+        el: '#app',
+        data: {
+            countries: []
+        },
+        mounted() {
+            this.getCountry();
+        },
+        methods: {
+            getCountry: function() {
+                fetch("https://restcountries.eu/rest/v2/all")
+                .then(response => response.json())
+                .then(data => this.countries = data)
+                .catch(console.error);
+            }
+        }
+    })
+})
